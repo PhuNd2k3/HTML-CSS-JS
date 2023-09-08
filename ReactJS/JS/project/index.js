15aa50d66a83d763060c6e8887a5774b398b50bf
Nguyen Duc Phu <phunguyenduc99@gmail.com>
2023-09-08 18:39:23 +0700

    push again:

---
+++ ReactJS/JS/project/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+app.use(morgan('combined'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ ReactJS/ReactJS/index.js
@@ -0,0 +1,5 @@
+import hello from "./index_2.js"
+import { hello_2 } from "./index_2.js"
+
+hello()
+hello_2()
